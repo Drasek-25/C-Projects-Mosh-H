f198fc58e0f501f33f7bd573010f9e41baebd49c
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2022-03-24 21:48:07 -0700

    Added project StackOverflowPost

---
+++ Ultimate-C#-Part-2/StackOverflowPost/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflowPost
+{
+    class Post
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Created { get; private set; }
+        public int Votes { get; private set; }
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Created = DateTime.Now;
+            this.Votes = 0;
+        }
+
+        public void Upvote()
+        {
+            this.Votes++;
+            Console.WriteLine("Current Votes: {0}", Votes);
+        }
+        public void Downvote()
+        {
+            this.Votes--;
+            Console.WriteLine("Current Votes: {0}", Votes);
+        }
+    }
+}
---
+++ Ultimate-C#-Part-2/StackOverflowPost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post("Test Post Please Ignore", "This is just a Test Post");
+            post1.Upvote();
+            post1.Upvote();
+            post1.Upvote();
+            post1.Upvote();
+            post1.Upvote();
+            post1.Downvote();
+            post1.Downvote();
+            Console.WriteLine(post1.Title);
+            Console.WriteLine(post1.Description);
+        }
+    }
+}
