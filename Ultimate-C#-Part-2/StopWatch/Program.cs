5abaf41b372c9da2263d400533aee857bb719170
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2022-03-24 21:30:14 -0700

    Added stopwatch excersize

---
+++ Ultimate-C#-Part-2/StopWatch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            Console.WriteLine(stopWatch.Start());
+            Console.WriteLine(stopWatch.Stop());
+        }
+    }
+}
---
+++ Ultimate-C#-Part-2/StopWatch/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime _start;
+        private bool _running = false;
+
+        public string Start()
+        {
+            if (_running == true)
+            {
+                throw new Exception("Stopwatch currently running.");
+            }
+            else
+            {
+                this._start = DateTime.Now;
+                this._running = true;
+                return "Started!";
+            }
+        }
+
+        public TimeSpan Stop()
+        {
+            if (_running == true)
+            {
+                _running = false;
+                return DateTime.Now - _start;
+            }
+            else;
+            {
+                throw new Exception("Stopwatch is not currently running.");
+            }
+        }
+    }
+}
